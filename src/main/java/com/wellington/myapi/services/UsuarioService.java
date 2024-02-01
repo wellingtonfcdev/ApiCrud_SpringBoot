f2a5105191abbc2cac773fe13107eee2a09ae604
WellingtonDev <wellingtonfc95@gmail.com>
2024-02-01 06:00:35 -0300

    Implementacao endPoint Delete

--- src/main/java/com/wellington/myapi/services/UsuarioService.java
+++ src/main/java/com/wellington/myapi/services/UsuarioService.java
@@ -37,6 +37,12 @@ public class UsuarioService {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+
+	public void delete(Integer id) {
+
+		findById(id);
+		repository.deleteById(id);
+	}
 	
 	
 	
